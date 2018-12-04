02b9657a745abbed13d66331577e71c804ecd48d
Sam Grill <samgrill@Sams-MacBook-Pro.local>
2018-12-04 11:31:08 +0000

    make the HTML have dynamic access to gif

--- src/main/java/IntroToSpring/Control/GifController.java
+++ src/main/java/IntroToSpring/Control/GifController.java
@@ -1,20 +1,24 @@
 package IntroToSpring.Control;
 
+import IntroToSpring.Model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller //Gets detected during Spring package scanning.
+import java.time.LocalDate;
+
+@Controller
 
 public class GifController {
-    @RequestMapping("/") //requests a URI
+    @RequestMapping("/")
     public String listGifs(){
         return "home";
     }
 
     @RequestMapping("/gif")
-    @ResponseBody
-    public String aNewResponse(){
-        return "A New Response";
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13),"Sam Grill", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
